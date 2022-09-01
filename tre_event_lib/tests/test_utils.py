4281833ef9e0c7337ed4e72f1ed7a9a78608be7d
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-09-01 19:32:31 +0100

    Update tests

--- tre_event_lib/tests/test_utils.py
+++ tre_event_lib/tests/test_utils.py
@@ -3,7 +3,6 @@ import sys
 import json
 
 def load_test_event(event_file_name: str) -> dict:
-    print(sys.path[0])
     test_event_dir = 'test_events'
     file_path = os.path.join(sys.path[0], test_event_dir, event_file_name)
     with open(file_path, 'rt', encoding='utf-8') as fp:
