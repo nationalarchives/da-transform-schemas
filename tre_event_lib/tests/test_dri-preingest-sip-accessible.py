dd6cc59c5692b607a20c4d32ee883bb88974f2f1
tna-aford <alex.ford@nationalarchives.gov.uk>
2022-09-02 14:09:06 +0100

    Initial Draft

---
+++ tre_event_lib/tests/test_dri-preingest-sip-accessible.py
@@ -0,0 +1,34 @@
+"""
+Tests for dri-preingest-sip-accessible event.
+"""
+import unittest
+import tre_event_lib
+import test_utils
+import jsonschema
+
+
+event_valid = test_utils.load_test_event(
+    event_file_name='dri-preingest-sip-accessible.json'
+)
+
+event_invalid_event_name = test_utils.load_test_event(
+    event_file_name='dri-preingest-sip-accessible-error.json'
+)
+
+EVENT_NAME = 'dri-preingest-sip-accessible'
+
+
+class TestDirPreingestSipAcessibleSchema(unittest.TestCase):
+    def test_event_valid(self):
+        tre_event_lib.validate_event(event=event_valid)
+
+    def test_event_invalid_event_name(self):
+        try:
+            tre_event_lib.validate_event(
+                event=event_invalid_event_name,
+                event_name=EVENT_NAME)
+            
+            self.fail('Did not get expected exception')
+        except jsonschema.exceptions.ValidationError as e:
+            expected = "'oops' is not one of ['new-bagit']"
+            self.assertTrue(expected in str(e))
