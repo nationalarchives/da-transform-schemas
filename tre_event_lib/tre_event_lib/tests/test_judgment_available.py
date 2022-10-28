46c10d9e538ffda78972b1bc4f8838001614b97a
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-10-28 18:16:00 +0100

    Add judgment-available event

---
+++ tre_event_lib/tre_event_lib/tests/test_judgment_available.py
@@ -0,0 +1,36 @@
+"""
+Tests for judgment-available event.
+"""
+import unittest
+import test_utils
+import jsonschema
+from tre_event_lib import tre_event_api
+
+
+event_valid = test_utils.load_test_event(
+    event_file_name='judgment-available.json'
+)
+
+event_invalid_event_name = test_utils.load_test_event(
+    event_file_name='judgment-available-invalid-event-name.json')
+
+EVENT_NAME = 'judgment-available'
+
+
+class TestJudgmentAvailable(unittest.TestCase):
+    """Tests for judgment-available event."""
+    def test_event_valid(self):
+        """Test judgment-available schema."""
+        tre_event_api.validate_event(event=event_valid)
+
+    def test_event_invalid_event_name(self):
+        """Test judgment-available schema fails when event-name invalid."""
+        try:
+            tre_event_api.validate_event(
+                event=event_invalid_event_name,
+                schema_name=EVENT_NAME)  # intended schema (as name invalid)
+            
+            self.fail('Did not get expected exception')
+        except jsonschema.exceptions.ValidationError as validation_error:
+            expected = "'invalid-event-name-test' is not one of ['judgment-available']"
+            self.assertTrue(expected in str(validation_error))
