142c7b3ccc7ef4510353fac55701f9ccee884dd3
Ian Hoyle <ian.hoyle.tna@gmail.com>
2023-04-19 14:55:41 +0100

    test relase

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --global user.email "${{ steps.import-gpg.outputs.email }}"
-git config --global user.name "${{ steps.import-gpg.outputs.name }}"
+#git config --global user.email "${{ steps.import-gpg.outputs.email }}"
+#git config --global user.name "${{ steps.import-gpg.outputs.name }}"
 git checkout -b $BRANCH_NAME
 git push -u origin $BRANCH_NAME
